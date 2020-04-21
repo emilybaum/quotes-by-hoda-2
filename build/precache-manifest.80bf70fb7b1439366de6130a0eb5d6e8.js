2948c0697b31d480ac40db652e3aa4c0d8d3e383
Emily Baumgartner <emily.r.baum@gmail.com>
2020-04-20 21:09:23 -0400

    initial commit

---
+++ build/precache-manifest.80bf70fb7b1439366de6130a0eb5d6e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "def3f2de6a154b0c7129724b139a855b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f1e4c63d0e22bd36f4e",
+    "url": "/static/css/main.ff10473a.chunk.css"
+  },
+  {
+    "revision": "4b42ca726ec414472134",
+    "url": "/static/js/2.a15242a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a15242a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f1e4c63d0e22bd36f4e",
+    "url": "/static/js/main.31fabd83.chunk.js"
+  },
+  {
+    "revision": "5f826b7510f74c644692",
+    "url": "/static/js/runtime-main.2ebbf447.js"
+  },
+  {
+    "revision": "230f9734fe74d175d2660c74e9c230b1",
+    "url": "/static/media/news_art_lifestyle_culture_41st.230f9734.png"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import './App.css';
+import logo from './img/news_art_lifestyle_culture_41st.png'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+      <title>News &amp; Documentary Emmy Awards New Approaches - Arts, Lifestyle &amp; Culture</title>
+      </header>
+
+        <table width="600" border="0" cellpadding="0" cellspacing="0" align="center">
+          <tr>
+            <td width="600" height="80" valign="top" style={{border: "none"}}>
+              <p><a href="http://www.emmyonline.tv/" target="_blank" rel="noopener noreferrer"><img src={logo}
+                    alt="New Approaches Emmy Award" width="600" height="105" border="0" /></a></p>
+            </td>
+          </tr>
+        </table>
+
+        <p>
+          <br />
+          <br />
+        </p>
+
+      <p>&nbsp;</p>
+      <p align="center">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/ewPXQsVdtcw" title="Misty Copeland Shares How She Deals With Critics And Trolls | Quoted By With Hoda | TODAY Originals" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </p>
+      <p>&nbsp;</p>
+      <p><br />
+      </p>
+      <table class="submission-table" align="center">
+        <tr>
+          <td class="define-left text-right">
+            Entry Category:
+          </td>
+          <td class="define-right">
+            Category #39: Outstanding New Approaches Arts, Lifestyle &amp; Culture
+          </td>
+        </tr>
+
+        <tr>
+          <td class="text-right">
+            Title 1 (ex. FRONTLINE):</td>
+          <td>
+            Quoted By...with Hoda
+          </td>
+        </tr>
+
+        <tr>
+          <td class="text-right">
+            Title 2 (ex. Ebola Outbreak: A Virtual Journey):
+          </td>
+          <td>
+            Misty Copeland
+          </td>
+        </tr>
+
+        <tr>
+          <td class="text-right">
+            Running Time:
+          </td>
+          <td>
+            00:09:05
+          </td>
+        </tr>
+
+        <tr>
+          <td class="text-right">
+            Production Company:</td>
+          <td>
+            NBC NEWS
+          </td>
+        </tr>
+
+        <tr>
+          <td class="text-right">
+            Date content was originally made available for viewing (must be 2019):
+          </td>
+          <td >
+            December 4, 2019
+          </td>
+        </tr>
+
+        <tr>
+          <td class="text-right">
+            Original URL (if applicable):
+          </td>
+          <td >
+              <a target="_blank" rel="noopener noreferrer" href=" https://www.today.com/video/misty-copeland-on-being-a-role-model-it-s-not-me-it-s-what-i-represent-74494021934">https://www.today.com/video/misty-copeland-on-being-a-role-model-it-s-not-me-it-s-what-i-represent-74494021934</a>
+          </td>
+        </tr>
+
+        <tr>
+          <td class="text-right" style={{verticalAlign: "top"}} >Essay:</td>
+          <td>
+            Hoda&#39;s digital series, Quoted By...with Hoda, shares a celebrity&#39;s story through their favorite quotes. Words of Wisdom, quotes or advice has always been important to Hoda. She posts them on her Instagram, wrote a few books with quotes and we decided to ignite a digital series around her passion. Hoda has found you learn so much about a person by their favorite quotes and how they have used these quotes throughout the ups and downs of his or her life. This episode focuses on Misty Copeland. Misty shares what she believes she represents in the ballet community, how her life was not picture perfect to prima ballerina and the role model she is to young girls. The first goal of the piece is for Misty to share her quotes but the added layer of this is how these quotes have been used throughout her life. Hoda exceeds both goals as our anchor by getting to the core of who Misty is through the quotes she has provided and how they represent her many years ago but also today. From an editing perspective, we want the viewer to really grasp what is being put in front of them. As most of us are visual learners, it made sense for us to write the quotes in the background that they are speaking about and add important life lessons throughout that Misty speaks about. This has been an easy concept for our viewers to get behind and they have shared it makes it easier for them to take these quotes into their own lives. They have also mentioned how it makes their favorite celebrity&#39;s more relatable. We all share a love of the same quotes so our viewers like to see that their favorite stars need the same words of wisdom. With the focus on quotes, we try not to distract the viewer with other elements. We felt the white background really lent itself to the quotes standing out and being the center of attention. Misty has done so many interviews before this but like most other interviews in a question and answer format. The quote leading the conversation format means we don&#39;t always know what we are going to get in the interview, but each one has become an organic conversation that is personal to who the person is and the struggles they go through. In this episode from two quotes she discusses her tough childhood, people like her not being represented in ballet, what the arts did for her, who loved her into being, her confidence and much more. We have over 25 episodes in this series and counting. Our audience loves it and we believe it is very different than anything else out there on a digital platform.
+            </td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default App;
